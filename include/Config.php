58aba1855000a8833698a13fe609f12b15fa8e90
Kamil Walas <kamilarturwalas@gmail.com>
2017-05-09 09:36:08 +0200

    initial commit, customized sample SLIM code

---
+++ include/Config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'kartwal_eGuide');
+define('DB_PASSWORD', 'eventGuide');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'kartwal_eGuide');
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
